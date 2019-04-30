11040dd0ae867031fad60dc1d157c4e2fc0682f4
carinadidomenico <carinadidomenico@gmail.com>
2019-04-30 16:22:11 -0300

    To Do List

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,49 @@
-let digitar = document.querySelector ("#digitar");
-let add = document.querySelector ("#add");
-let tarefas = document.querySelector ("#tarefas");
+let texto = document.querySelector('#texto');
+let btn = document.querySelector('#btn');
+let marcar = document.querySelector ('#marcar');
+let limpar = document.querySelector ('#limpar');
+let tarefas = document.querySelector('#tarefas ul');
 
-add.addEventListener ('click', function(){
-    tarefas.innerHTML += `<p> ${digitar.value} </p>`;
-    
+//Adicionar as tarefas
 
+btn.addEventListener('click', function () {
+    if (texto.value == false)
+    return;
+    tarefas.innerHTML += `<li class="item" id="li" onclick="ticar(this)"> ${texto.value} <span onclick="excluir(this)"><img class="fechar" src="assets/img/error.png" alt="fechar"><span></li>`;
+    texto.value = "";
+    opcoes.style.display = 'flex';
 })
+
+// Função riscar a tarefa
+
+function ticar(elemento) {
+    
+    elemento.classList.toggle ('ticarTarefa');
+}
+
+//Função clicar no x e apagar um elemento
+
+function excluir(elemento) {
+    elemento.parentElement.remove();
+}  
+
+//Apagar todos os elementos
+
+limpar.addEventListener ('click', function(){   
+    tarefas.innerHTML = '<ul></ul>';
+    limpar.checked = false;   
+})
+
+//Marcar todos 
+
+marcar.addEventListener ('click', function(){
+
+    let item = document.querySelectorAll ('li');
+    item.forEach(function(elemento){
+        elemento.classList.toggle ('ticarTarefa');
+        marcar.checked = false; 
+    })
+    
+})
+
+
