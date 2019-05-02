8c3b56757e6d586c69fe07de68fdbefe3802834a
carinadidomenico <carinadidomenico@gmail.com>
2019-05-02 16:03:01 -0300

    To Do List

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,28 @@
 let texto = document.querySelector('#texto');
 let btn = document.querySelector('#btn');
+let form = document.querySelector ('#form');
 let marcar = document.querySelector ('#marcar');
 let limpar = document.querySelector ('#limpar');
 let tarefas = document.querySelector('#tarefas ul');
+let item = document.querySelectorAll ('#li')
 
 //Adicionar as tarefas
 
-btn.addEventListener('click', function () {
+form.addEventListener('submit', function (evento) {
+    evento.preventDefault();
     if (texto.value == false)
     return;
-    tarefas.innerHTML += `<li  class="item" id="li" onclick="ticar(this)"> ${texto.value} <span onclick="excluir(this)"><img class="fechar" src="assets/img/error.png" alt="fechar"><span></li>`;
+    tarefas.innerHTML += `<li draggable="true" class="item" id="li" onclick="ticar(this)"> ${texto.value} <span onclick="excluir(this)"><img class="fechar" src="assets/img/error.png" alt="fechar"><span></li>`;
     texto.value = "";
     opcoes.style.display = 'flex';
+    btn.style.borderRadius = '0';
 })
 
 // Função riscar a tarefa
 
 function ticar(elemento) {
     
-    elemento.classList.toggle ('ticarTarefa');
+    elemento.classList.toggle('ticarTarefa');
 }
 
 //Função clicar no x e apagar um elemento
@@ -31,17 +35,54 @@ function excluir(elemento) {
 
 limpar.addEventListener ('click', function(){   
     tarefas.innerHTML = '<ul></ul>';
-    limpar.checked = false;   
+    limpar.checked = false;
+    opcoes.style.display = 'none';
+    btn.style.borderRadius = '0 0 10px 10px';
 })
 
-//Marcar todos 
+//Marcar todos
 
-marcar.addEventListener ('click', function(){
+marcarTodas.addEventListener ('click', function(){
 
     let item = document.querySelectorAll ('li');
     item.forEach(function(elemento){
         elemento.classList.toggle ('ticarTarefa');
-        marcar.checked = false; 
+        marcarTodas.checked = false; 
     });
-})
+});
 
+//Arrastar
+
+
+tarefas.addEventListener("dragstart", dragStart);
+
+tarefas.addEventListener("dragover", dragOver);
+
+tarefas.addEventListener("dragend", dragEnd);
+
+
+// Arrastando
+
+function dragStart (evento) {
+    dragging = evento.target.closest(".item");
+    item.style.opacity = '0.9';
+    item.style.backgroundColor = "#ba7af8";
+}
+
+
+// Largar
+
+function dragOver(evento) {
+    evento.preventDefault ();
+    const location = evento.target.closest(".item");
+    this.insertBefore(dragging, location);
+    
+    // this. equivale a class timeline
+}
+
+function dragEnd(evento) {
+    dragging = null;
+    this.style.backgroundColor = "white";
+    //usamos o dragging = null para dizer que deve parar o movimento
+
+}
